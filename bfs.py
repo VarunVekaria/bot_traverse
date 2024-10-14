324869608ce9741994f00b351ad0eeaae18618bc
Varun <varunvekaria@gmail.com>
2024-10-14 00:43:43 -0400

    readme changes

--- bfs.py
+++ bfs.py
@@ -5,4 +5,4 @@ from pygame_interface import run_pygame_gui
 # bot_called= 3 calls Bot 3, 
 # bot_called= 4 calls Bot 4, 
 # bot_called= 5, is the bonus case where ship environment is also changing. 
-run_pygame_gui(40, q=1, bot_called= 5)
+run_pygame_gui(40, q=random.random, bot_called= 5)
